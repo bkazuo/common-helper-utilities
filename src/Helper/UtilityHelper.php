4b61f0e0289342afea5cf06aa9d09f07bee3e5ca
Bruno Takahashi <bruno.kazuo90@gmail.com>
2017-03-06 17:01:36 -0300

    Added new method that removes special characters

--- src/Helper/UtilityHelper.php
+++ src/Helper/UtilityHelper.php
@@ -233,4 +233,17 @@ class UtilityHelper {
         $time2 = strtotime($closingTime);
         return round(abs($time2 - $time1) / 3600,2);
     }
+
+    /**
+     * Transform any special charactor into simple character
+     *
+     * @param string $nameWithSpecialCharacter
+     */    
+    function removeSpecialCharacter($name) {
+        $lowerName = strtolower($name);
+        $what = array( 'ä','ã','à','á','â','ê','ë','è','é','ï','ì','í','ö','õ','ò','ó','ô','ü','ù','ú','û','À','Á','É','Í','Ó','Ú','ñ','Ñ','ç','Ç',' ','-','(',')',',',';',':','|','!','"','#','$','%','&','/','=','?','~','^','>','<','ª','º', "'" );
+        $by   = array( 'a','a','a','a','a','e','e','e','e','i','i','i','o','o','o','o','o','u','u','u','u','A','A','E','I','O','U','n','n','c','C','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_', '_');
+
+        return str_replace($what, $by, $lowerName);
+    }
 }
